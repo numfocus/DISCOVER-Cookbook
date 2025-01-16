46ec28903d9ad655e4aeea042b64b2191e16f5e6
Ritesh Singh <ritesh.davv@gmail.com>
2025-01-16 20:29:50 +0530

    Create updateLogo func and a MutationObserver to update the mainLogo src

--- DISCOVER/_static/logo-switcher.js
+++ DISCOVER/_static/logo-switcher.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const logos = document.querySelectorAll('.logo__image');
-    
+
     logos.forEach(logo => {
       if (logo.classList.contains('only-light')) {
         logo.src = '_static/logo-light.png'; // Replacing with the light logo
@@ -8,4 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
         logo.src = '_static/logo-dark.png'; // Replacing with the dark logo
       }
     });
+
+    function updateLogo() {
+      let mainLogo = document.querySelector(".bd-content img:not(.only-dark,.dark-light)"); // Select the image
+      let isDarkMode = document.documentElement.getAttribute("data-theme") === "dark"; // Check theme
+    
+      if (mainLogo) {
+          mainLogo.src = isDarkMode ? "_static/logo-dark.png" : "_static/logo-light.png"; // Change image source
+      }
+    }
+    
+    // Listen for attribute changes on <html> that is the root node of the document
+    const observer = new MutationObserver(updateLogo);
+    observer.observe(document.documentElement, { attributes: true, attributeFilter: ["data-theme"] });
+    updateLogo();
 });
