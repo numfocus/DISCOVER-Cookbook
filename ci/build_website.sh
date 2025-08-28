2b9fc10a683f699518fca620447f59be2902acf5
Ashish Mahajan <mahajanashishar21sm@gmail.com>
2025-08-29 00:20:46 +0530

    update: workflows/2.0-translations (#388)
    * update:workflows/2.0-translations
    
    * remove: pr preview

--- ci/build_website.sh
+++ ci/build_website.sh
@@ -2,7 +2,7 @@
 echo "Starting build process..."
 
 # Get parameters with defaults 
-VERSION=${VERSION:-${1:-"2.0"}}
+VERSION=${VERSION:-${1:-"dev"}}
 LANGUAGE=${LANGUAGE:-${2:-"en"}}
 
 echo "Building version: $VERSION, language: $LANGUAGE"
